a892835cd7afde744fbdf28831f01539dbb1337e
smomop <63437136+smomop@users.noreply.github.com>
2026-03-07 00:31:07 +0800

    Update sing-box.js

--- 1.11.x/sing-box.js
+++ 1.11.x/sing-box.js
@@ -16,7 +16,24 @@ let proxies = await produceArtifact({
 config.outbounds.push(...proxies)
 
 config.outbounds.map(i => {
-  i.outbounds.push(...getTags(proxies))
+  if (['✈️ 节点选择', '♻ 自动选择'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies))
+  }
+  if (['hk', 'hk-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /港|hk|hongkong|hong kong|🇭🇰/i))
+  }
+  if (['tw', 'tw-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /台|tw|taiwan|🇹🇼/i))
+  }
+  if (['jp', 'jp-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /日本|jp|japan|🇯🇵/i))
+  }
+  if (['sg', 'sg-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^(?!.*(?:us)).*(新|sg|singapore|🇸🇬)/i))
+  }
+  if (['us', 'us-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /美|us|unitedstates|united states|🇺🇸/i))
+  }
 })
 
 config.outbounds.forEach(outbound => {
